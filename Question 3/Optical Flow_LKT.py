75dd23c25196d045319a9d47ab296485cd8e04f6
rakshith <rakshith21@iiserb.ac.in>
2023-10-26 21:26:31 +0530

    V2

--- Question 3/Optical Flow_LKT.py
+++ Question 3/Optical Flow_LKT.py
@@ -1,14 +1,18 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture("Intesection2.mp4")  # Replace with your video source
+cap = cv2.VideoCapture("Traffic_intersection.mp4")  # Replace with your video source
 
 # Parameters for Lucas-Kanade optical flow
 lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
 ret, prev_frame = cap.read()
 prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
-prev_pts = cv2.goodFeaturesToTrack(prev_gray, mask=None, maxCorners=1000, qualityLevel=0.3, minDistance=5)
+prev_pts = cv2.goodFeaturesToTrack(prev_gray, mask=None, maxCorners=3000, qualityLevel=0.3, minDistance=3)
+
+# Define the VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Corrected this line
+out = cv2.VideoWriter('output_video.avi', fourcc, 30, (int(cap.get(3)), int(cap.get(4))))
 
 while True:
     ret, frame = cap.read()
@@ -35,11 +39,15 @@ while True:
     prev_gray = frame_gray.copy()
     prev_pts = good_new.reshape(-1, 1, 2)
 
+    # Write the frame with optical flow to the output video
+    out.write(frame)
+
     # Display the video frame with optical flow tracks
     cv2.imshow("Optical Flow (Lucas-Kanade)", frame)
-    cv2.imwrite("Opt_flow.mp4", frame)
+
     if cv2.waitKey(30) & 0xFF == 27:
         break
 
+# Release the VideoWriter and capture objects
+out.release()
 cap.release()
-cv2.destroyAllWindows()
